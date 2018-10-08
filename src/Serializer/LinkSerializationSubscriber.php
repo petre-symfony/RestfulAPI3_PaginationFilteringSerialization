6aafa8fe33934fef51f52b33315b4560c2eeb6bb
petrero <petrero@hotmail.com>
2018-10-08 06:04:35 -0500

    I mentioned that there are two ways to add super-custom fields like uri. Using a serializer subscriber is the first. But sometimes, your API representation will look much different than your entity. Imagine we had some crazy endpoint that returned info about a Programmer mixed with details about their last 3 battles, the last time they fought and the current weather in their hometown.
    Can you imagine trying to do this? You'll need multiple @VirtualProperty methods and probably some craziness inside an event subscriber. It might work, but it'll look ugly and be confusing.
    
    In this case, there's a much better way: create a new class with the exact properties you need. Then, instantiate it, populate the object in your controller and serialize it. This class isn't an entity - it's just there to model your API response. I love this approach and recommend it as soon as you're doing more than just a few serialization customizations to a class.

--- src/Serializer/LinkSerializationSubscriber.php
+++ src/Serializer/LinkSerializationSubscriber.php
@@ -2,10 +2,22 @@
 namespace App\Serializer;
 
 
+use App\Entity\Programmer;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\JsonSerializationVisitor;
+use Symfony\Component\Routing\RouterInterface;
 
 class LinkSerializationSubscriber implements EventSubscriberInterface {
+	/**
+	 * @var RouterInterface
+	 */
+	private $router;
+
+	public function __construct(RouterInterface $router){
+		$this->router = $router;
+	}
+
 	public static function getSubscribedEvents(){
 		return [
 			[
@@ -18,6 +30,18 @@ class LinkSerializationSubscriber implements EventSubscriberInterface {
 	}
 
 	public function onPostSerialize(ObjectEvent $event){
-
+		/** @var JsonSerializationVisitor $visitor */
+		$visitor = $event->getVisitor();
+		/** @var Programmer $programmer */
+		$programmer = $event->getObject();
+		$visitor->addData(
+			'uri',
+			$this->router->generate(
+				'api_programmers_show',
+				[
+					'nickname' => $programmer->getNickname()
+				]
+			)
+		);
 	}
 }
